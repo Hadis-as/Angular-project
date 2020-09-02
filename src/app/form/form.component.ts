f05638a4da564177d57e26cea0a76b0491ec7cd2
hadise asemani <asemanihadise@gmail.com>
2020-09-02 19:16:06 +0430

    secon commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,13 +11,13 @@ export class FormComponent {
     company: null,
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
-    address: [null, Validators.required],
+    email: [null, Validators.required],
     address2: null,
     city: [null, Validators.required],
     state: [null, Validators.required],
-    // postalCode: [null, Validators.compose([
-    //   Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    // ],
+    postalCode: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
+    ],
     shipping: ['free', Validators.required]
   });
 
